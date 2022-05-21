16ae4c31c4e1e1646f704f484f8dc604c5e2affe
JeongHoon.Han <yuria2005@gmail.com>
2022-05-21 09:25:55 +0900

    더미 적용 20220521

--- OTT/src/component/Body2.js
+++ OTT/src/component/Body2.js
@@ -2,16 +2,63 @@ import React from 'react'
 
 import MovieMain from './MovieMain'
 import MovieGroup from './MovieGroup'
+import dummy from "../db/data.json";
 
 const Body = () => {
+    console.log(dummy)
+    console.log(dummy["NetflixOriginals"])
+    console.log(dummy["tv"])
     
     return (
         <div className='container'>
-            <MovieMain />
-        <div className='movieGroup'>
-            <p>movieGroup</p>
+            <MovieMain movie={null} />
+            <div className='movieShowcase'>
+                <MovieGroup
+                  isNetflixMovies={true}
+                  title='Netflix Originals'
+                  selectMovieHandler={null}
+                  movies={dummy["NetflixOriginals"]["results"]}
+                />
+                <MovieGroup
+                    title='Trending'
+                    selectMovieHandler={null}
+                    movies={dummy["Trending"]["results"]}
+                />
+                <MovieGroup
+                    title='Top Rated'
+                    selectMovieHandler={null}
+                    movies={dummy["TopRated"]["results"]}
+                />
+                <MovieGroup
+                    title='Action Movies'
+                    selectMovieHandler={null}
+                    movies={dummy["ActionMovies"]["results"]}
+                />
+                <MovieGroup
+                    title='Comedy'
+                    selectMovieHandler={null}
+                    movies={dummy["ComedyMovies"]["results"]}
+                />
+                <MovieGroup
+                    title='Horror Movies'
+                    selectMovieHandler={null}
+                    movies={dummy["HorrorMovies"]["results"]}
+                />
+                <MovieGroup
+                    title='Romance'
+                    selectMovieHandler={null}
+                    movies={dummy["RomanceMovies"]["results"]}
+                />
+                <MovieGroup
+                    title='Documentaries'
+                    selectMovieHandler={null}
+                    movies={dummy["Documentaries"]["results"]}
+                />
+            </div>
+            {/* <div className='movieGroup'>
+                <p>movieGroup</p>
+            </div> */}
         </div>
-      </div>
     )
 }
 
