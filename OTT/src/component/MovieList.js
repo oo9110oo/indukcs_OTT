3ee96976052ad8e51ace1dd8c2c9d4adb8fab56b
kjw99 <lkk7209@naver.com>
2022-06-14 18:52:35 +0900

    0614

--- OTT/src/component/MovieList.js
+++ OTT/src/component/MovieList.js
@@ -5,14 +5,14 @@ const MovieList = () => {
 
   const listDelete = (e) => {
     if(window.confirm("정말 삭제하시겠습니까?")) {
-      localStorage.removeItem(e)
-      window.location.reload();
+      localStorage.removeItem(e)      
+      window.location.reload(); // 새로고침. 새로고침 안하면 화면이 그대로 남아있어서
     }
     
   }
 
   const rendering = () => {
-    let movies = localStorage.getItem('list');
+    let movies = localStorage.getItem('list'); // myList에 저장된 영상 개수
     const result = [];
     if (movies == null) {
       result.push(<div>
@@ -20,9 +20,10 @@ const MovieList = () => {
       </div>)
       return result;
     } else {
-      for (let i = 0; i <= movies; i++) {
-        if(localStorage.getItem(i) != null) {
+      for (let i = 0; i <= movies; i++) { // 저장된 영상들의 key는 다 숫자임
+        if(localStorage.getItem(i) != null) { // 영상 데이터 있는지 확인
           const title = JSON.parse(localStorage.getItem(i))[0]['title'] || JSON.parse(localStorage.getItem(i))[0]['name'];
+          // 영상들 중에서 제목이 title에 저장되는거랑 name에 저장되는게 있어서 둘 중 하나만 있어도 가져오도록 설정
           const imagePath = 'https://image.tmdb.org/t/p/original/' + JSON.parse(localStorage.getItem(i))[0]['backdrop_path'];
           result.push(<div className='addMovieList' key={title}>          
             <img className='movieShowcase__container--movie-image' src={imagePath} style={{width:'200px'}} />
