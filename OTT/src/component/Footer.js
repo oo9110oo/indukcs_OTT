396f32e049d301d14f0b9d133c249969f227f51f
K4rpeDieM <pony1125@naver.com>
2022-06-15 18:31:56 +0900

    footer 수정

--- OTT/src/component/Footer.js
+++ OTT/src/component/Footer.js
@@ -1,8 +1,43 @@
+// import React from 'react'
+
+// const footer = () => (
+//   <footer className='footer'>
+//     <div className='footer__copyright'>
+//       &copy; 2022 Made by{' '}
+//       <a
+//         className='footer__copyright--link'
+//         href='https://github.com/oo9110oo/indukcs_OTT.git'
+//       >
+//         {' '}
+//         DUCKING
+//       </a>
+//     </div>
+//   </footer>
+// )
+
+// export default footer
+
+
 import React from 'react'
+import "../static/scss/footer.scss"
+import DuckingLogo from '../static/images/ducking_full.png'
 
 const footer = () => (
   <footer className='footer'>
-    <div className='footer__copyright'>
+    <div id="footer_box">
+      <div id="footer_logo">
+        <img src={DuckingLogo} style={{width:'200px', position: 'relative'}}/>       
+      </div>
+      
+      
+
+      <div id ="address">
+        <ul>
+          <li>서울시 노원구 초안산로 12</li>
+          <li>TEL : 02-950-7000 Email : oo9110oo@naver.com</li>
+          <li>COPYRIGHT (C) 집가고싶조 ALL RIGHTS RESERVED</li>
+          <li>
+          <div className='footer__copyright'>
       &copy; 2022 Made by{' '}
       <a
         className='footer__copyright--link'
@@ -12,7 +47,14 @@ const footer = () => (
         DUCKING
       </a>
     </div>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
   </footer>
 )
+
+
 
 export default footer
